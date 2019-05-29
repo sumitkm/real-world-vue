fde192d9ff4fd27842f24059b613b686a89636ea
Sumit Kumar Maitra <sumitkm@gmail.com>
2019-05-29 18:01:57 +0100

    Snake cased components with scss separated out

--- src/components/hello-world/hello-world.vue
+++ src/components/hello-world/hello-world.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      First modification to Vue JS Hello World <br/>
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
@@ -40,21 +39,6 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped lang="scss" src="./hello-world.scss"></style>
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App"></hello-world>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/hello-world/hello-world.vue'; // @ is an alias to /src
 
 @Component({
   components: {
